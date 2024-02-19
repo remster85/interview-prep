1146c24d1451b68fbd2117c9bb56cb2194e218ce
remster85 <remi.desreumaux@gmail.com>
2024-02-19 10:51:42 -0500

    Post contest 385

--- algos/leetcode/dijkstrashortreach.py
+++ algos/leetcode/dijkstrashortreach.py
@@ -2,31 +2,63 @@
 
 import os
 import heapq
+import math
+from io import StringIO
 
+def show_tree(tree, total_width=60, fill=' '):
+    """Pretty-print a tree.
+    total_width depends on your input size"""
+    output = StringIO()
+    last_row = -1
+    for i, n in enumerate(tree):
+        if i:
+            row = int(math.floor(math.log(i+1, 2)))
+        else:
+            row = 0
+        if row != last_row:
+            output.write('\n')
+        columns = 2**row
+        col_width = int(math.floor((total_width * 1.0) / columns))
+        output.write(str(n).center(col_width, fill))
+        last_row = row
+    print (output.getvalue())
+    print ('-' * total_width)
+    return
 
 def dijkstra(graph, start): 
+    print(graph)
     # Initialize the distances 
     distances = {vertex: float('infinity') for vertex in graph} 
     distances[start] = 0
-     
+    print(distances)
+
     # Create a priority queue 
     priority_queue = [(0, start)] 
      
     while priority_queue: 
         # Extract the vertex with the smallest distance 
+        show_tree(priority_queue)        
         current_distance, current_vertex = heapq.heappop(priority_queue) 
-         
+        print(f"popping{current_distance},{current_vertex} tab={distances}")
+        print(distances)
+        show_tree(priority_queue)
+
+        
         # Check if we have found a shorter path 
         if current_distance > distances[current_vertex]: 
+            print('continue')
             continue 
          
         # Update the distance of the neighbours 
         for neighbour, weight in graph[current_vertex].items(): 
             distance = current_distance + weight 
+            print(f"check {neighbour} {weight} : distance = {current_distance} + {weight} = {distance} vs  neighbor {neighbour}:" + str(distances[neighbour]))
             if distance < distances[neighbour]: 
                 distances[neighbour] = distance 
-                print(f"pushing to queue {(distance, neighbour)}")
+                print(f"pushing to queue {(distance, neighbour)} tab={distances}")
                 heapq.heappush(priority_queue, (distance, neighbour)) 
+                print(distances)
+                show_tree(priority_queue)
                  
     return distances 
 
@@ -58,27 +90,7 @@ def shortestReach(n, edges, s):
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    t = int(input().strip())
+    edges = [[1,2,24],[1,4,20],[3,1,3],[4,3,12]]
+    print(shortestReach(1, edges, 1))
 
-    for t_itr in range(t):
-        first_multiple_input = input().rstrip().split()
-
-        n = int(first_multiple_input[0])
-
-        m = int(first_multiple_input[1])
-
-        edges = []
-
-        for _ in range(m):
-            edges.append(list(map(int, input().rstrip().split())))
-
-        s = int(input().strip())
-
-        result = shortestReach(n, edges, s)
-
-        fptr.write(' '.join(map(str, result)))
-        fptr.write('\n')
-
-    fptr.close()
