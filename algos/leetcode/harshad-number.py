4f824ff7e9ab2f4a8ae0fbafa7cb24ffbe33ff50
remster85 <remi.desreumaux@gmail.com>
2024-04-11 20:08:24 -0400

    harshard-number

---
+++ algos/leetcode/harshad-number.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/harshad-number
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        
+        sum = 0
+        for c in str(x):
+            sum += int(c)
+        fraction = x / sum
+        return -1 if fraction - int(fraction) != 0 else sum
+        
