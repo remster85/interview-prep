f84c1eb06f413950fa426f35e290b0c47934ed38
remster85 <remi.desreumaux@gmail.com>
2024-10-12 18:22:07 -0400

    Create validpalindromeii.py

---
+++ algos/leetcode/validpalindromeii.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/valid-palindrome-ii/
+class Solution:
+
+    def isStrictPalindrome(self, s: str) -> bool:
+        print("checking isStrictPalindrome " + s)
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        deletions = 0
+    
+        while i < j  and deletions < 2:
+            if s[i] != s[j]:
+                if deletions == 1:
+                    return False
+                if  s[i+1] == s[j] and s[i] == s[j-1]:
+                    t1 = self.isStrictPalindrome(s[i:j])
+                    t2 = self.isStrictPalindrome(s[i+1:j+1])
+                    return t1 or t2
+                elif s[i+1] == s[j]:
+                    i+=1
+                elif s[i] == s[j-1]:
+                    j-=1 
+                else:
+                    return False
+                deletions += 1
+            else:    
+                i+=1
+                j-=1
+        return deletions < 2
